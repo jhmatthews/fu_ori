19b78c1cabb8db063f0a9b5cd638ee2db25b5d82
jhmatthews <jm8g08@soton.ac.uk>
2017-03-31 16:08:31 +0100

    added spectra

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
 import py_read_output as r 
 import matplotlib.pyplot as pl
 
-s = r.read_spectrum("yso_a")
+#put name of model here
+MODEL_NAME = "fu_ori_alpha4"
+s = r.read_spectrum(MODEL_NAME)
 
 pl.plot(s["Lambda"], s["A85P0.50"])
-pl.show()
+pl.savefig("{}_spectrum.png".format(MODEL_NAME))
